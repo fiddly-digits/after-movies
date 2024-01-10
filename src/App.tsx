f2105a62beadffec09b6b6064425d0bb2b9db296
fiddly-digits <rob.crlu@gmail.com>
2024-01-09 18:05:55 -0600

    feat: movielist and movie card design finished

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { Router } from './router/Router';
 import { Header, Footer } from './components';
 import './App.css';
 
+//! Check if the theme in the main div is the most adequate solution
+
 function App() {
   return (
-    <div>
+    <div className='bg-white dark:bg-gray-800'>
       <Header />
       <Router />
       <Footer />
--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -1,7 +1,18 @@
+import { MovieCard } from '../components';
+
 export function MovieList() {
   return (
     <main>
-      <div className='text-7xl'>Movie List</div>
+      <section className='mx-auto max-w-7xl py-7'>
+        <div className='flex flex-wrap justify-start'>
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+        </div>
+      </section>
     </main>
   );
 }
